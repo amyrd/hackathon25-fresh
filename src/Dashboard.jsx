2632eebee17d2f116d09c98bea965c56f313913b
Phillip Pham <phpham014@gmail.com>
2025-02-08 15:13:48 -0600

    news ticker bar

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -5,6 +5,32 @@ import Posts from "./Posts.jsx";
 
 function Dashboard() {
   const [posts, setPosts] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const travelDestinations = [
+    // "Japan #1",
+    // "Italy #2",
+    // "New Zealand #3",
+    // "Canada #4",
+    // "Switzerland #5",
+    // "Greece #6",
+    // "Australia #7",
+    // "Thailand #8",
+    // "France #9",
+    // "Iceland #10",
+
+    "#1 Japan",
+    "#2 Italy",
+    "#3 New Zealand",
+    "#4 Canada",
+    "#5 Switzerland",
+    "#6 Greece",
+    "#7 Australia",
+    "#8 Thailand",
+    "#9 France",
+    "#10 Iceland",
+  ]
+
 
   useEffect(() => {
     fetch("http://localhost:8080/twitter/posts") // Fetch data from Go backend
@@ -43,10 +69,21 @@ function Dashboard() {
             <h3>Stats</h3>
           </div>
         </div>
-        <div className="row-container">Footer element</div>
+      </div>
+
+     {/* News Ticker */}
+     <div className="ticker-bar">
+        <div className="news-ticker">
+          <div className="ticker-content">
+            {travelDestinations.map((country, index) => (
+              <span key={index} className="ticker-item">
+                {country} &nbsp; | &nbsp;
+              </span>
+            ))}
+          </div>
+        </div>
       </div>
     </>
   );
 }
-
 export default Dashboard;
