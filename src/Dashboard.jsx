34fc62a3ab8562381ce632b0d27e93b2a9dbe43e
Brandon Thach <brandonthach17@gmail.com>
2025-02-08 16:44:48 -0600

    forms

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,113 +1,25 @@
-// import { Menubar } from "primereact/menubar";
-// import SparkLogo from "./assets/sparkhacks-logo.svg";
-// import { useEffect, useState } from "react";
-// import Insta from "./Insta.jsx";
-// import Twitter from "./Twitter.jsx";
-// import Stats from "./Stats.jsx";
-// import { Button } from "primereact/button";
-// import { Dialog } from "primereact/dialog";
-
-// function Dashboard() {
-//   const postInsta = () => {};
-//   const [visible, setVisible] = useState(false);
-
-//   const postTweet = () => {};
-
-//   const travelDestinations = [
-//     "TRENDING TAGS:",
-//     "#business",
-//     "#digitalmarketing",
-//     "#entrepreneur",
-//     "#onlinebusiness",
-//     "#marketing",
-//     "#store",
-//     "#ecommerce",
-//     "#smallbusiness",
-//     "#shopping",
-//     "#sales",
-//   ];
-//   return (
-//     <>
-//       <div className="navbar">
-//         <img id="logo" src={SparkLogo} />
-//         <h1>MyBusinessBoard : Social Media Manager</h1>
-//       </div>
-//       <div className="body-container">
-//         <div className="column-container">
-//           <div className="post-container">
-//             <div className="posts">
-//               {" "}
-//               <Insta />
-//             </div>
-//             <Button
-//               className="post-button"
-//               type="button"
-//               onClick={() => setVisible(true)}
-//             >
-//               Create Instagram Post ⊕
-//             </Button>
-//             <Dialog
-//               header="Create an Instagram Post"
-//               visible={visible}
-//               draggable={false}
-//               className="createInstaPost"
-//               modal={false}
-//               style={{ width: "50vw" }}
-//               onHide={() => {
-//                 if (!visible) return;
-//                 setVisible(false);
-//               }}
-//             >
-//               <p className="m-0">
-//                 <form>
-
-//                 </form>
-//               </p>
-//             </Dialog>
-//           </div>
-//           <div className="post-container">
-//             <div className="posts">
-//               {" "}
-//               <Twitter />
-//             </div>
-//             <Button className="post-button" type="button">
-//               Create Twitter Post ⊕
-//             </Button>
-//           </div>
-//           <div className="stats">
-//             <Stats />
-//           </div>
-//         </div>
-//         <div className="ticker-bar">
-//           <div className="news-ticker">
-//             <div className="ticker-content">
-//               {travelDestinations.map((country, index) => (
-//                 <span key={index} className="ticker-item">
-//                   {country} &nbsp;&nbsp;
-//                 </span>
-//               ))}
-//             </div>
-//           </div>
-//         </div>{" "}
-//       </div>
-//     </>
-//   );
-// }
-
-// export default Dashboard;
-
 import { Menubar } from "primereact/menubar";
 import SparkLogo from "./assets/sparkhacks-logo.svg";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Insta from "./Insta.jsx";
 import Twitter from "./Twitter.jsx";
 import Stats from "./Stats.jsx";
 import { Button } from "primereact/button";
 import { Dialog } from "primereact/dialog";
+import { FileUpload } from "primereact/fileupload";
+import { Toast } from "primereact/toast";
 
 function Dashboard() {
   const [instaVisible, setInstaVisible] = useState(false);
   const [twitterVisible, setTwitterVisible] = useState(false);
+  const toast = useRef(null);
+  const onUpload = () => {
+    toast.current.show({
+      severity: "info",
+      summary: "Success",
+      detail: "File Uploaded",
+    });
+  };
 
   const travelDestinations = [
     "TRENDING TAGS:",
@@ -153,7 +65,12 @@ function Dashboard() {
               onHide={() => setInstaVisible(false)}
             >
               <form>
-                <p>Instagram post form goes here</p>
+                Enter a caption: <input></input>
+                <div className="card flex justify-content-center">
+            <Toast ref={toast}></Toast>
+            <FileUpload mode="basic" name="demo[]" url="/api/upload" accept="image/*" maxFileSize={1000000} onUpload={onUpload} auto chooseLabel="Browse Files" />
+        </div>  
+        <Button type="Submit">Submit</Button>
               </form>
             </Dialog>
           </div>
@@ -181,6 +98,19 @@ function Dashboard() {
             >
               <form>
                 <p>Twitter post form goes here</p>
+                <div className="card flex justify-content-center">
+                  <Toast ref={toast}></Toast>
+                  <FileUpload
+                    mode="basic"
+                    name="demo[]"
+                    url="/api/upload"
+                    accept="image/*"
+                    maxFileSize={1000000}
+                    onUpload={onUpload}
+                    auto
+                    chooseLabel="Browse"
+                  />
+                </div>
               </form>
             </Dialog>
           </div>
